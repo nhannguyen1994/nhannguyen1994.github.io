5c1682b934c493ef4abb537be7f81349107140a2
nhannguyen1994 <nhank57tha@gmail.com>
2017-02-20 21:53:35 +0700

    vòng lặp bảng cuu chương
    javascript

---
+++ bai1.js
@@ -0,0 +1,83 @@
+n=prompt("Nhap");
+if(n==2) {
+ console.log("BANG CUU CHUONG 2");
+    console.log("");
+
+    for(j=1;j<=9;j++)
+    {
+
+        console.log(j," x 2 = ",+2*j);
+    }
+
+}
+if(n==3) {
+ console.log("BẢNG CỬU CHƯƠNG 3");
+    console.log("");
+
+    for(j=1;j<=9;j++)
+    {
+
+        console.log("<br>"," 3 x ",j,"=",+3*j);
+    }
+
+}
+if(n==4) {
+ console.log("BANG CUU CHUONG 4");
+    console.log("");
+    for(j=1;j<=9;j++)
+    {
+
+        console.log("<br>",j," x 4 = ",+4*j);
+    }
+
+}
+if(n==5) {
+ console.log("BANG CUU CHUONG 5");
+    console.log("");
+    for(j=1;j<=9;j++)
+    {
+
+        console.log("<br>",j," x 5 = ",+5*j);
+    }
+
+}
+else
+if(n==6) {
+ console.log("BANG CUU CHUONG 6");
+    console.log("");
+    for(j=1;j<=9;j++)
+    {
+
+        console.log("<br>",j," x 6 = ",+6*j);
+    }
+
+}
+if(n==7)
+{console.log("BANG CUU CHUONG 7");
+    console.log("");
+    for(j=1;j<=9;j++)
+    {
+
+        console.log(j," x 7 = ",+7*j);
+    }
+
+}
+if(n==8) {
+ console.log("BANG CUU CHUONG 8");
+    for(j=1;j<=9;j++)
+    {
+
+        console.log("<br>",j," x 8 = ",+8*j);
+    }
+
+}
+if(n==9) {
+ console.log("BANG CUU CHUONG 9");
+    console.log("");
+    for(j=1;j<=9;j++)
+    {
+
+        console.log("<br>",j," x 9 = ",9*j);
+    }
+
+}

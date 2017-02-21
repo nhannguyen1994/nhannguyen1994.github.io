8e55976df18a9e13c275bc03cf0c47a3e8a10ba5
nhannguyen1994 <nhank57tha@gmail.com>
2017-02-21 09:35:45 +0700

    bang cuu chuong
    ban chuan

--- bai1.js
+++ bai1.js
@@ -1,83 +1,71 @@
-n=prompt("Nhap");
-if(n==2) {
- console.log("BANG CUU CHUONG 2");
-    console.log("");
+function bangcuuchuong() {
+    for(var n = 0; n<=10;n++){
+        if (n == 1) {
+            console.log("BANG CUU CHUONG 1");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 1= ",+1* i);
+            }
+        }
 
-    for(j=1;j<=9;j++)
-    {
+        if (n == 2) {
+            console.log("BANG CUU CHUONG 2");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 2= ",+2* i);
+            }
+        }
 
-        console.log(j," x 2 = ",+2*j);
+        if (n == 3) {
+            console.log("BANG CUU CHUONG 3");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 3= ",+3 * i);
+            }
+        }
+        if (n == 4) {
+            console.log("BANG CUU CHUONG 4");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 4= ",+4 * i);
+            }
+
+        }
+        if (n == 5) {
+            console.log("BANG CUU CHUONG 5");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 5= ",+5 * i);
+            }
+        }
+        if (n == 6) {
+            console.log("BANG CUU CHUONG 6");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 6= ",+6 * i);
+            }
+        }
+        if (n == 7) {
+            console.log("BANG CUU CHUONG 7");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 7 = ",+7 * i);
+            }
+        }
+        if (n == 8) {
+            console.log("BANG CUU CHUONG 8");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 8  = ",+8 * i);
+            }
+        }
+        if (n == 9) {
+            console.log("BANG CUU CHUONG 9");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 9  = ",+9 * i);
+            }
+        }
+        if (n == 10) {
+            console.log("BANG CUU CHUONG 10");
+            for (i = 1; i <= 10; i++) {
+                console.log(i + "* 10  = ",+10 * i);
+            }
+        }
     }
 
 }
-if(n==3) {
- console.log("BẢNG CỬU CHƯƠNG 3");
-    console.log("");
 
-    for(j=1;j<=9;j++)
-    {
+bangcuuchuong();
 
-        console.log("<br>"," 3 x ",j,"=",+3*j);
-    }
-
-}
-if(n==4) {
- console.log("BANG CUU CHUONG 4");
-    console.log("");
-    for(j=1;j<=9;j++)
-    {
-
-        console.log("<br>",j," x 4 = ",+4*j);
-    }
-
-}
-if(n==5) {
- console.log("BANG CUU CHUONG 5");
-    console.log("");
-    for(j=1;j<=9;j++)
-    {
-
-        console.log("<br>",j," x 5 = ",+5*j);
-    }
-
-}
-else
-if(n==6) {
- console.log("BANG CUU CHUONG 6");
-    console.log("");
-    for(j=1;j<=9;j++)
-    {
-
-        console.log("<br>",j," x 6 = ",+6*j);
-    }
-
-}
-if(n==7)
-{console.log("BANG CUU CHUONG 7");
-    console.log("");
-    for(j=1;j<=9;j++)
-    {
-
-        console.log(j," x 7 = ",+7*j);
-    }
-
-}
-if(n==8) {
- console.log("BANG CUU CHUONG 8");
-    for(j=1;j<=9;j++)
-    {
-
-        console.log("<br>",j," x 8 = ",+8*j);
-    }
-
-}
-if(n==9) {
- console.log("BANG CUU CHUONG 9");
-    console.log("");
-    for(j=1;j<=9;j++)
-    {
-
-        console.log("<br>",j," x 9 = ",9*j);
-    }
-
-}
